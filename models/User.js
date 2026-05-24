d2bdc0a507e1883274925865fb2ce5db03861de8
Anas Javed <anasmughalonline@gmail.com>
2026-05-25 02:38:04 +0500

    Refactor userSchema definition in User.js

--- models/User.js
+++ models/User.js
@@ -1,33 +1,29 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema(
-  role: {
-  type: String,
-  enum: ['customer', 'seller'],
-  default: 'customer'
-}
-  {
-    name: {
-      type: String,
-      required: [true, "Name is required"],
-      trim: true,
-    },
-    email: {
-      type: String,
-      required: [true, "Email is required"],
-      unique: true,
-      lowercase: true,
-      trim: true,
-    },
-    password: {
-      type: String,
-      required: [true, "Password is required"],
-      minlength: 6,
-    },
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true
   },
-  {
-    timestamps: true,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['customer', 'seller'],
+    default: 'customer'
   }
-);
+}, { 
+  timestamps: true 
+});
 
 export default mongoose.model("User", userSchema);
